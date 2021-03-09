1d1e920434850b30dc6b2294ea0ff8988fe31f3a
E-SeongHo <lsh031255@naver.com>
2021-03-09 20:56:41 +0900

    Create Button

---
+++ Assets/Scripts/ButtonArrayMaker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonArrayMaker : MonoBehaviour
+{
+    public GameObject button_red;
+    public GameObject button_green;
+    public GameObject button_blue;
+    Rigidbody2D rigidbody2d;
+    GameObject button;
+    bool button_ON = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!button_ON)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                GiveButton2();
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.C))
+            {
+                
+                button_ON = false;
+            }    
+        }
+        
+    }
+    void GiveButton1()
+    {
+        GameObject who = GameObject.Find("Enemy");
+        rigidbody2d = who.GetComponent<Rigidbody2D>();
+        Vector2 position = rigidbody2d.position;
+        button = Instantiate(button_red,position + Vector2.up*2f,
+        Quaternion.identity);
+        button_ON=true;
+    } 
+    void GiveButton2()
+    {
+        GameObject who = GameObject.Find("Enemy");
+        button = button_red;
+        EnemyController enemyController = who.GetComponent<EnemyController>();
+        enemyController.GenerateButton(button,'r');
+        button_ON=true;
+    }
+    void DeleteButton()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 1f;
+    GameObject myButton;
+    char myColor;
+
+    //THIS OBJECT'S KEY
+    //Red button key = A
+    //Green button key = S
+    //Blue button key = D
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1*speed*Time.deltaTime,0,0);
+
+    }
+    public void GenerateButton(GameObject button, char color)
+    {
+        Vector2 position = GetComponent<Rigidbody2D>().position;
+        myButton = Instantiate(button, position + Vector2.up * 1.5f, Quaternion.identity);
+        myButton.transform.parent = gameObject.transform;
+        myColor = color;    
+    }
+    public void TryDelete(char colorKey)
+    {
+        //입력된 키가 뭔지 판단해서 해당 객체의 키와 일치하면 딜리트 허용
+        if()
+        myButton = null;
+    }
+}
