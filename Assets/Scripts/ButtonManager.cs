c6ef6f9ce686b4fe583c6d980e670629171119d5
E-SeongHo <lsh031255@naver.com>
2021-03-09 23:02:00 +0900

    Button Delete With Right KEY

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject button_red;
+    public GameObject button_green;
+    public GameObject button_blue;
+    Rigidbody2D rigidbody2d;
+    GameObject button;
+    bool button_ON = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!button_ON) // 현재 프레임에 어떤 오브젝트에도 버튼이 떠있지 않으면
+        {
+            if(Input.GetMouseButtonDown(0)) // 오브젝트중 가장 가까운 객체 선정
+            {
+                GiveButton2(); // 해당 오브젝트에 버튼 생성
+            }
+        }
+        else // 현재 프레임에 어떤 오브젝트에 버튼이 떠있으면
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                DeleteButton('A');
+                Debug.Log("A");
+            } 
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                DeleteButton('S');
+                Debug.Log("S");
+            }
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                DeleteButton('D');
+                Debug.Log("D");
+            }  
+        }
+    }
+    void GiveButton1()
+    {
+        GameObject who = GameObject.Find("Enemy");
+        rigidbody2d = who.GetComponent<Rigidbody2D>();
+        Vector2 position = rigidbody2d.position;
+        button = Instantiate(button_red,position + Vector2.up*2f,
+        Quaternion.identity);
+        button_ON = true;
+    } 
+    void GiveButton2()
+    {
+        GameObject who = GameObject.Find("Enemy");
+        button = button_red;
+        EnemyController enemyController = who.GetComponent<EnemyController>();
+        enemyController.GenerateButton(button,'r');
+        button_ON = true;
+    }
+    void DeleteButton(char button)
+    {
+        GameObject who = GameObject.Find("Enemy");
+        EnemyController enemyController = who.GetComponent<EnemyController>();
+        // TryDelete : Delete 성공시 true 반환하므로 Delete 성공 시 button_ON = false로 설정
+        button_ON = !(enemyController.TryDelete(button));
+    }
+
+
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed = 1f;
-    GameObject myButton;
+    // 버튼 생성이 Enemy객체에 필요하면 myButton 오브젝트에 할당
+    // Enemy의 자식객체로 구현하여 Enemy객체와 같이 움직이도록 구현
+    GameObject myButton; 
+
     char myColor;
 
     //THIS OBJECT'S KEY
@@ -26,17 +29,41 @@ public class EnemyController : MonoBehaviour
         transform.Translate(-1*speed*Time.deltaTime,0,0);
 
     }
+    static char ConvertColorToKey(char color)
+    {   
+        // myColor를 실인자로 주는 것을 원칙
+        switch(color)
+        {
+            case 'r':
+                return 'A';
+            case 'g':
+                return 'S';
+            case 'b':
+                return 'D';
+        }
+        return 'F'; // 예외경우 대비(그럴 일 없는데 컴파일 에러나니까 했다)
+    }
     public void GenerateButton(GameObject button, char color)
     {
         Vector2 position = GetComponent<Rigidbody2D>().position;
         myButton = Instantiate(button, position + Vector2.up * 1.5f, Quaternion.identity);
         myButton.transform.parent = gameObject.transform;
-        myColor = color;    
+        myColor = color;    // 버튼의 색상 저장 -> Delete에 이용
+
     }
-    public void TryDelete(char colorKey)
+    public bool TryDelete(char colorKey)
     {
-        //입력된 키가 뭔지 판단해서 해당 객체의 키와 일치하면 딜리트 허용
-        if()
-        myButton = null;
+        //입력된 키가 뭔지 판단해서 이 객체의 해당 색깔 키와 일치하면 딜리트 허용
+        if (colorKey == ConvertColorToKey(myColor))
+        {
+            //올바른 키 입력이면 Delete 허용
+
+            //myButton = null;
+            Destroy(myButton); // Destory보다는 null로 처리하고 안보이게 하는게 좋을듯
+
+            //버튼이 삭제 되었으면 true 반환
+            return true;
+        }
+        else return false; // 올바르지 않은 키 입력이면 fasle 반환
     }
 }
